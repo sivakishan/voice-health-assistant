5de8834fe4c77982ef19cd039b1174417acefeb5
Beesa Sivakishan Atal Bihari <69931519+sivakishan@users.noreply.github.com>
2025-04-21 15:49:15 +0200

    Update twilio_handler.py

--- twilio_handler.py
+++ twilio_handler.py
@@ -1,13 +1,18 @@
+import os
 from twilio.rest import Client
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 
 def make_reservation_call(to_number, medicine_name):
-    account_sid = 'your-twilio-sid'
-    auth_token = 'your-twilio-token'
-    client = Client(account_sid, auth_token)
+    account_sid = 'ACee661d696689f319bc0eb22d0f8a6cfb'
+    auth_token = 'e3cc53e447d4c1f7eceb979feec38ea8'
+    from_number = os.getenv('+13608432103')
 
+    client = Client(account_sid, auth_token)
     call = client.calls.create(
         twiml=f'<Response><Say>A user wants to reserve {medicine_name}. Please confirm.</Say></Response>',
         to=to_number,
-        from_='your-twilio-verified-number'
+        from_=from_number
     )
     return call.sid
